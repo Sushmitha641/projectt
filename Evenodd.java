1b5833d727f534684bf5e16e6b97d01091c9e47f
Sushmitha641 <245122748027@mvsrec.edu.in>
2025-03-18 13:14:30 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,14 @@
+public class Evenodd {
+    public static void main(String[] args) {
+        // Number to be checked
+        int number = 7; // You can change this number to test others
+
+        // Check if the number is even or odd
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
+
